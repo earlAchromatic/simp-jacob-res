307e739d307cbc62752c5dcb9b077a97d2003fab
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2021-09-22 22:38:58 -0400

    first commit & site scaffold

---
+++ js/techList.js
@@ -0,0 +1,66 @@
+function getTechList() {
+  return {
+    techlist: [
+      [
+        {
+          imgUrl: "./images/tech/html5.png",
+          title: "HTML5",
+          blurb:
+            "Every rock solid dev foundation starts with understanding hypertext",
+        },
+        {
+          imgUrl: "./images/tech/js.png",
+          title: "Javascript",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/ts-logo-512.png",
+          title: "Javascript",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/node.png",
+          title: "Node.js",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/sass.png",
+          title: "Sass",
+          blurb: "",
+        },
+      ],
+      [
+        {
+          imgUrl: "./images/tech/vue.png",
+          title: "Vue 2",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/vue.png",
+          title: "Vue 3",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/web-component.png",
+          title: "Web Component",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/alpine_long.png",
+          title: "Alpine.js",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/bulma-logo.png",
+          title: "Bulma",
+          blurb: "",
+        },
+        {
+          imgUrl: "./images/tech/halfmoon.png",
+          title: "Halfmoon",
+          blurb: "css framework",
+        },
+      ],
+    ],
+  };
+}
